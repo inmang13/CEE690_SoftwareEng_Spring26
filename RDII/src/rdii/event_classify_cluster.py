dfa6dfb58cc09915b0ab7937895bd228dbcfe9e0
inmang13 <inmang2000@gmail.com>
2026-03-03 18:26:01 -0500

    outlining next steps

---
+++ RDII/src/rdii/event_classify_cluster.py
@@ -0,0 +1,19 @@
+"""
+event_classify.py
+
+Purpose:
+    Classify flow response events using physically interpretable
+    axes derived from event statistics.
+"""
+
+# Read Event Statistics
+# Select one feature per concpetual axis
+# Normal or standarize axes if needed
+# cluster
+    # k-means
+    # heirarcical Clisting
+    # gusssian mixture models
+    # sensative to scaling and outliers
+    # exploratory
+#assign repsonse class label
+# flag ambiguous or borderline events
